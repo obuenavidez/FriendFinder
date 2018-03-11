243f07208fd60bf721e11ca0dbf9260623f79a3c
Oscar Buenavidez <oscarbuenavidez@Oscars-MacBook-Pro.local>
2018-03-11 04:58:56 -0500

    friend finder v1.0

---
+++ app/data/friend.js
@@ -0,0 +1,20 @@
+var friendList = [
+    {
+      name: 'Oscar Buenavidez',
+      profilePic: 'https://www.pexels.com/photo/gray-scale-photo-of-topless-man-covering-face-235355/',
+      scores: [
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
